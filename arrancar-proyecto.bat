8205a040c89dbf068ac9f091762d1ab42897367b
Mario Villagra <mvillagra.lorca@gmail.com>
2024-12-22 15:20:31 -0300

    add - arrancar proyecto

---
+++ arrancar-proyecto.bat
@@ -0,0 +1,3 @@
+start cmd.exe /k "cd C:\Program Files\MongoDB\Server\8.0\bin && mongod.exe"
+
+start cmd.exe /k "cd C:\wamp64\www\api-rest-red-social && npm start"
---
+++ index.js
@@ -0,0 +1,11 @@
+//Conexion a bbdd
+
+//crear servidor node
+
+//configurar cors
+
+//convertir los datos del body a objetos js
+
+//cargar conf rutas
+
+//poner servidor a escuchar peticiones http
